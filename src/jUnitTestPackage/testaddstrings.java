4186a5fb0f83d7331ba32bfb71c5c81e85314954
admin <admin@10.12.59.193>
2022-11-21 14:43:43 +0530

    jUnitTest

---
+++ src/jUnitTestPackage/testaddstrings.java
@@ -0,0 +1,14 @@
+package jUnitTestPackage;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class testaddstrings {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ testaddnumbers.java
@@ -0,0 +1,14 @@
+package jUnitTestPackage;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class testaddnumbers{
+
+	@Test
+	public void test() {
+		jUnitFunctions junit=new jUnitFunctions();
+		int result=junit.Addnumbers(100,200);
+		assertEquals(300,result);
+	}
+}
